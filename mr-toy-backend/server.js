119ddbd07253fb4b2f7bc097672e2f80e4935da7
LigalLev <ligalelias@gmail.com>
2023-05-23 17:20:58 +0300

    s

--- mr-toy-backend/server.js
+++ mr-toy-backend/server.js
@@ -3,20 +3,30 @@ const app = express()
 const toyService = require('./services/toy-service')
 const cookieParser = require('cookie-parser')
 const path = require('path')
-const cors = require('cors') 
+const cors = require('cors')
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
 
 
 // App Configuration
-const corsOptions = {
-    origin: [
-        'http://127.0.0.1:8080',
-        'http://localhost:8080',
-        'http://127.0.0.1:3000',
-        'http://localhost:3000'
-    ],
-    credentials: true
-}
-app.use(cors(corsOptions))
+// const corsOptions = {
+//     origin: [
+//         'http://127.0.0.1:8080',
+//         'http://localhost:8080',
+//         'http://127.0.0.1:3000',
+//         'http://localhost:3000'
+//     ],
+//     credentials: true
+// }
+// app.use(cors(corsOptions))
 app.use(express.static('public'))
 app.use(cookieParser()) // for res.cookies
 app.use(express.json()) // for req.body
@@ -107,9 +117,9 @@ app.get('/api/toy/:toyId', (req, res) => {
 app.delete('/api/toy/:toyId', (req, res) => {
     const { toyId } = req.params
     toyService.remove(toyId)
-    .then(msg => {
-        res.send({ msg, toyId })
-    })
+        .then(msg => {
+            res.send({ msg, toyId })
+        })
         .catch(err => {
             console.log('Cannot remove toy')
             res.status(400).send('Cannot remove toy')
@@ -122,5 +132,13 @@ app.get('/**', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.listen(3030, () => console.log('Server ready at port 3030!'))
+// app.listen(3030, () => console.log('Server ready at port 3030!'))
+
+const port = process.env.PORT || 3030;
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+});
 
